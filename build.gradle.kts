4e48c28aceed4285a77edeb7600c5e04b39e0dd6
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-12-22 12:09:03 +0530

    remove all extras

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
 @file:Suppress("UnstableApiUsage")
 
-import com.vanniktech.maven.publish.SonatypeHost
-import org.gradle.plugin.devel.GradlePluginDevelopmentExtension // <--- THIS WAS MISSING
-
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
     alias(libs.plugins.vanniktech.maven.publish)
 }
-
-val groupProp = project.findProperty("GROUP") as String
-val versionProp = project.findProperty("VERSION_NAME") as String
-
-group = groupProp
-version = versionProp
 
 java {
     toolchain {
@@ -28,15 +19,11 @@ dependencies {
 }
 
 configure<GradlePluginDevelopmentExtension> {
-    website.set(project.findProperty("POM_URL") as String)
-    vcsUrl.set(project.findProperty("POM_SCM_URL") as String)
 
     plugins {
         create("apkDistPlugin") {
             id = "io.github.mlm-games.apk-dist"
             implementationClass = "org.mlm.ApkDistPlugin"
-            displayName = project.findProperty("POM_NAME") as String
-            description = project.findProperty("POM_DESCRIPTION") as String
             tags.set(listOf("android", "apk", "distribution"))
         }
     }
@@ -45,5 +32,4 @@ configure<GradlePluginDevelopmentExtension> {
 mavenPublishing {
     publishToMavenCentral()
     signAllPublications()
-    coordinates(groupProp, "apk-dist-plugin", versionProp)
 }
